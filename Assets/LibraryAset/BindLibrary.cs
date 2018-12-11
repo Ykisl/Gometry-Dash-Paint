6d0f977d4747619f0f320e194c7c662f8787d530
Ykisl <yakhs2014@mail.ru>
2018-12-12 02:10:52 +0700

    LibraryFIX

---
+++ Assets/LibraryAset/BindLibrary.cs
@@ -0,0 +1,49 @@
+﻿using GeometryDashAPI.Levels;
+using GeometryDashAPI.Levels.Enums;
+using GeometryDashAPI.Levels.GameObjects.Default;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BindLibrary : MonoBehaviour {
+
+    public BindingBlockID binding;
+
+    void LoadBlocks()
+    {
+        binding = new BindingBlockID();
+        binding.Bind(1887, typeof(Bush01));
+    }
+
+    public List<int> GetFilter()
+    {
+        List<int> filter = new List<int>();
+        filter.Add(1887);
+        return filter;
+    }
+    public BindingBlockID GetBindingLB()
+    {
+        LoadBlocks();
+        return binding;
+    }
+}
+
+class Bush01 : DetailBlock
+{
+    public override Layer Default_ZLayer { get; protected set; } = Layer.B2;
+    public override short Default_ZOrder { get; protected set; } = 9;
+
+    public Bush01() : base(1887)
+    {
+    }
+
+    public Bush01(string[] data) : base(data)
+    {
+    }
+
+    public override string ToString()
+    {
+        return base.ToString();
+    }
+}
+
---
+++ Assets/LibraryAset/ObjLibrary.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjLibrary : MonoBehaviour {
+    public GameObject TestGameObj;
+    public GameObject BushCursor;
+    public GameObject bush01;
+
+    void Start () {
+	}
+
+    public GameObject SetGameObj(int id)
+    {
+        return GameObjLib(id);
+    }
+
+    public GameObject GameObjLib(int id)
+    {
+        GameObject LibSet;
+        switch (id)
+        {
+              case -1:
+                LibSet = TestGameObj;
+                break;
+            case -2:
+                LibSet = BushCursor;
+                break;
+            case 1887:
+                LibSet = bush01;
+                break;
+            default:
+                LibSet = null;
+                break;
+        }
+        return LibSet;
+    }
+}
